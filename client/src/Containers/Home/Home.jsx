2da647414fcf9940362053746f982d9ce81d42f0
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-02-25 13:38:31 -0500

    minor update

--- client/src/Containers/Home/Home.jsx
+++ client/src/Containers/Home/Home.jsx
@@ -5,7 +5,7 @@ import "../Home/Home.css";
 
 const Home = () => {
 	return (
-		<div class="parent">
+		<div>
 			<Navbar />
 
 			<section class="section is-medium">
@@ -27,36 +27,37 @@ const Home = () => {
 				</div>
 				<div class="column">
 					<div class="container is-fluid">
-						<div class="notification is-primary contain">
-							<div className="card">
-								<div className="card-image">
-									<figure className="image is-4by3">
-										<img
-											src="https://www.visitsavannah.com/sites/default/files/styles/large_square/public/original-marshall-house.jpg?itok=wT9khAzR"
-											alt="Marshall House"
-										/>
-									</figure>
+                        <div class="notification is-primary contain">
+                            <div className="card">
+						<div className="card-image">
+							<figure className="image is-4by3">
+								<img
+									src="https://www.visitsavannah.com/sites/default/files/styles/large_square/public/original-marshall-house.jpg?itok=wT9khAzR"
+									alt="Marshall House"
+								/>
+							</figure>
+						</div>
+						<div className="card-content">
+							<div className="media">
+								<div className="media-content is-centered mb-1">
+									<p className="title is-4">Marshall House</p>
+									<p className="subtitle is-6">@johnsmith</p>
 								</div>
-								<div className="card-content">
-									<div className="media">
-										<div className="media-content is-centered mb-1">
-											<p className="title is-4">Marshall House</p>
-											<p className="subtitle is-6">@johnsmith</p>
-										</div>
-									</div>
+							</div>
 
-									<div className="content">
-										123 E Broughton St <br></br>
-										Savannah, GA
-										<br></br>
-										<Link to={`/sightings/6036a2adf3f15e00287620fa`}>
-											Tell Me More
-										</Link>
-									</div>
-								</div>
+							<div className="content">
+								123 E Broughton St <br></br>
+								Savannah, GA
+								<br></br>
+								<Link to={`/sightings/6036a2adf3f15e00287620fa`}>
+									Tell Me More
+								</Link>
 							</div>
 						</div>
 					</div>
+                        </div>
+					</div>
+					
 				</div>
 			</div>
 		</div>
