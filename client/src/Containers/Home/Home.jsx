224852de54b7719b68ba4dea5fc24669700a6d5f
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-02-25 13:42:09 -0500

    formmating

--- client/src/Containers/Home/Home.jsx
+++ client/src/Containers/Home/Home.jsx
@@ -27,37 +27,36 @@ const Home = () => {
 				</div>
 				<div class="column">
 					<div class="container is-fluid">
-                        <div class="notification is-primary contain">
-                            <div className="card">
-						<div className="card-image">
-							<figure className="image is-4by3">
-								<img
-									src="https://www.visitsavannah.com/sites/default/files/styles/large_square/public/original-marshall-house.jpg?itok=wT9khAzR"
-									alt="Marshall House"
-								/>
-							</figure>
-						</div>
-						<div className="card-content">
-							<div className="media">
-								<div className="media-content is-centered mb-1">
-									<p className="title is-4">Marshall House</p>
-									<p className="subtitle is-6">@johnsmith</p>
+						<div class="notification is-primary contain">
+							<div className="card">
+								<div className="card-image">
+									<figure className="image is-4by3">
+										<img
+											src="https://www.visitsavannah.com/sites/default/files/styles/large_square/public/original-marshall-house.jpg?itok=wT9khAzR"
+											alt="Marshall House"
+										/>
+									</figure>
+								</div>
+								<div className="card-content">
+									<div className="media">
+										<div className="media-content is-centered mb-1">
+											<p className="title is-4">Marshall House</p>
+											<p className="subtitle is-6">@johnsmith</p>
+										</div>
+									</div>
+
+									<div className="content">
+										123 E Broughton St <br></br>
+										Savannah, GA
+										<br></br>
+										<Link to={`/sightings/6036a2adf3f15e00287620fa`}>
+											Tell Me More
+										</Link>
+									</div>
 								</div>
 							</div>
-
-							<div className="content">
-								123 E Broughton St <br></br>
-								Savannah, GA
-								<br></br>
-								<Link to={`/sightings/6036a2adf3f15e00287620fa`}>
-									Tell Me More
-								</Link>
-							</div>
 						</div>
 					</div>
-                        </div>
-					</div>
-					
 				</div>
 			</div>
 		</div>
