c838de64741c23edb626d31c6a6719dad991024a
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-25 14:03:27 -0500

    Finishing code for EditSighting.jsx

--- client/src/Containers/EditSighting/EditSighting.jsx
+++ client/src/Containers/EditSighting/EditSighting.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useHistory, useParams } from "react-router-dom";
-// import RestaurantForm from "../../components/RestaurantForm/RestaurantForm";
+// import SightingsForm from "../../components/SightingsForm/SightingsForm";
 
 const EditSighting = () => {
     const history = useHistory();
@@ -10,14 +10,26 @@ const EditSighting = () => {
     const handleFormSubmit = (e, formObject) => {
         e.preventDefault();
         axios
-          .put(`/api/restaurants/${id}`, formObject)
+          .put(`/api/sightings/${id}`, formObject)
           .then((response) => {
             console.log(response.data);
-            history.push(`/restaurants/${response.data._id}`);
+            history.push(`/sightings/${response.data._id}`);
           })
           .catch((err) => {
             console.log(err);
           });
       };
+
+      return (
+        <div className="container">
+          <div className="row">
+            <SightingsForm
+              handleFormSubmit={handleFormSubmit}
+              buttonText="Update"
+            />
+          </div>
+        </div>
+      );
+    };
 
 export default EditSighting;
