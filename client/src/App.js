6204a17a6c8f5867b9ce2c2714b72bbb08463806
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-25 13:55:50 -0500

    Incorporating EditSighting and handleFormSubmit functions to EditSighting.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import AddSighting from "./Containers/AddSighting/AddSighting";
 // import AddUser from "./Containers/AddUser/AddUser";
 import AllSightings from "./Containers/AllSightings/AllSightings";
-// import NavBar from "./Components/NavBar";
+import NavBar from "./Components/NavBar";
 // import AllUsers from "./Containers/AllUsers/AllUsers";
-// import EditSighting from "./Containers/EditSighting/EditSighting";
+import EditSighting from "./Containers/EditSighting/EditSighting";
 // import Home from "./Containers/Home/Home";
 // import SingleSighting from "./Containers/SingleSighting/SingleSighting";
 // import SingleUser from "./Containers/SingleUser/SingleUser";
@@ -15,13 +15,13 @@ function App() {
     <div className="App">
 
       <Router>
-      {/* <NavBar /> */}
+      <NavBar />
         <Switch>
           {/* <Route exact path="/" component={Home} /> */}
           <Route exact path="/sightings" component={AllSightings} />
           {/* <Route exact path="/sightings/new" component={AddSighting} /> */}
           {/* <Route exact path="/sightings/:id" component={SingleSighting} /> */}
-          {/* <Route exact path="/sightings/:id/edit" component={EditSighting} /> */}
+          <Route exact path="/sightings/:id/edit" component={EditSighting} />
           {/* <Route exact path="/users" component={AllUsers} /> */}
           {/* <Route exact path="/users/new" component={AddUser} /> */}
           {/* <Route exact path="/users/:id" component={SingleUser} /> */}
--- client/src/Containers/EditSighting/EditSighting.jsx
+++ client/src/Containers/EditSighting/EditSighting.jsx
@@ -1,7 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
+// import RestaurantForm from "../../components/RestaurantForm/RestaurantForm";
 
+const EditSighting = () => {
+    const history = useHistory();
+    const { id } = useParams();
 
+    const handleFormSubmit = (e, formObject) => {
+        e.preventDefault();
+        axios
+          .put(`/api/restaurants/${id}`, formObject)
+          .then((response) => {
+            console.log(response.data);
+            history.push(`/restaurants/${response.data._id}`);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
 
 export default EditSighting;
