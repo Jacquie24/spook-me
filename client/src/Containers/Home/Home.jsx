4d0091fe68db7b1443c39efe9720966f4a8f0949
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-02-25 11:30:05 -0500

    adding content sections and hero section to homepage

--- client/src/Containers/Home/Home.jsx
+++ client/src/Containers/Home/Home.jsx
@@ -1,11 +1,47 @@
 import React from "react";
+import Navbar from "../../Components/NavBar/NavBar";
 
 const Home = () => {
 	return (
 		<div>
-			<h1>This is my home page.</h1>
+			<Navbar />
+
+			<section class="section is-medium">
+				<h1 class="title">Spook Me</h1>
+				<h2 class="subtitle">
+					Don't forget your Holy Water...
+				</h2>
+			</section>
+
+			<div class="columns">
+				<div class="column">
+					<div class="container is-fluid">
+						<div class="notification is-primary">
+							This container is <strong>fluid</strong>: it will have a 32px gap
+							on either side, on any viewport size.
+						</div>
+					</div>
+				</div>
+				<div class="column">
+					<div class="container is-fluid">
+						<div class="notification is-primary">
+							This container is <strong>fluid</strong>: it will have a 32px gap
+							on either side, on any viewport size.
+						</div>
+					</div>
+					<br />
+					<div class="container is-fluid">
+						<div class="notification is-primary">
+							This container is <strong>fluid</strong>: it will have a 32px gap
+							on either side, on any viewport size.
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
+<styles>#88177a99</styles>;
