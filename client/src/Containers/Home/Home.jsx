63668683829066e340afa31acd54cb75b887f7bf
Jacquie24 <jgcaption@yahoo.com>
2021-02-26 13:54:18 -0500

    Cloudinary widget done working

--- client/src/Containers/Home/Home.jsx
+++ client/src/Containers/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Navbar from "../../Components/NavBar/NavBar";
-import "../Home/Home.css";
+import "./Home.css";
 
 const Home = () => {
 	return (
