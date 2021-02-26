45f894c9b635a602a656148a393b36501f8b1947
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-26 13:57:10 -0500

    Creating basic skeleton for AllUsers.jsx

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -19,7 +19,10 @@ module.exports = {
     db.User
       .create(req.body)
       .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .catch(err => {
+        console.log(err);
+        res.status(422).json(err)});
+      
   },
   update: function(req, res) {
     db.User
