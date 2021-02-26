243441ee9089c554aefdb07ae33da7f9de96666b
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-26 13:58:22 -0500

    Adding useState and useEffect functionality

--- client/src/Containers/AllUsers/AllUsers.jsx
+++ client/src/Containers/AllUsers/AllUsers.jsx
@@ -1,6 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+const AllUsers = () => {
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("/api/users")
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  
 
 export default AllUsers;
