4b2488b72f4cb35909ff66f1131667c43d847288
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-02-28 18:53:10 -0500

    navbar lookin better

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -6,12 +6,8 @@ const NavBar = () => {
 	return (
 		<nav className="navbar" role="navigation" aria-label="main navigation">
 			<div className="navbar-brand">
-				<a className="navbar-item" href="https://bulma.io">
-					<img
-						src="https://bulma.io/images/bulma-logo.png"
-						width="112"
-						height="28"
-					/>
+				<a className="navbar-item">
+					<Link to="/">SpookMe</Link>
 				</a>
 
 				<a
