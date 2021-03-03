9cd3f0d7dfdd5ee626f9e9e9818f54ab40da94d2
Juno Brown <amarbrown@gmail.com>
2021-03-03 10:30:04 -0500

    able to loop through data file

--- client/src/Components/GoogleMap/GoogleMap.js
+++ client/src/Components/GoogleMap/GoogleMap.js
@@ -2,35 +2,43 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import "./Map.css";
 import LocationMarker from "./LocationMarker";
-import * as HauntedPlaces from "./haunted_places.json"
+import HauntedPlaces from "./haunted_places.json"
 // import { Icon } from '@iconify/react'
 // import locationIcon from '@iconify/icons-mdi/map-marker'
 
 
-  
+
 
 
 const Map = ({ center, zoom }) => {
-    
-      return (
+
+  return (
     <div className="map">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyBMbV-_uofN5Ac1WcR0CY9_jwB-_zayCsg' }}
-          defaultCenter={ center }
-          defaultZoom={ zoom }
-        >
-          <LocationMarker lat={center.lat} lng={center.lng}/>
-          </GoogleMapReact>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyBMbV-_uofN5Ac1WcR0CY9_jwB-_zayCsg' }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+       {HauntedPlaces.map((haunting) => (
+          <LocationMarker key={haunting.index}
+              lat= {haunting.latitude}
+              lng= {haunting.longitude}
+          />
+        )) }
+
+         <LocationMarker lat={center.lat} lng={center.lng}/>
+
+      </GoogleMapReact>
     </div>
-    )
-    }
+  )
+}
 
-    Map.defaultProps = {
-      center: {
-      lat: 33.7489,
-      lng: -84.3879,
-      },
-      zoom: 6,
-  }
+Map.defaultProps = {
+  center: {
+    lat: 33.7489,
+    lng: -84.3879,
+  },
+  zoom: 6,
+}
 
-  export default Map;
+export default Map;
